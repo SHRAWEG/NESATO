f152185542cf047b7edb7487093260f385455f6b
SHRAWEG <swgmaharjan@gmail.com>
2021-07-28 14:42:37 +0545

    invitation update

--- src/components/home/elements/Team.js
+++ src/components/home/elements/Team.js
@@ -14,20 +14,24 @@ function Team(props) {
                         <FontAwesomeIcon icon={faUsers} className="text-3xl" /><h1 className="text-3xl font-bold ml-4">Teams</h1>
                     </div>
                     <div>
-                        {props.teams.map((team, key) => (
-                            <div key={key}>
-                                <h1 className="text-xl">{team.game}</h1>
-                                <Link href = {'/team/' + team._id } >
-                                    <a>
-                                        <p className="text-lg mb-4"><span className="pl-2">{team.team_name}</span></p>
-                                    </a>
-                                </Link>
-                            </div>
-                        ))}
+                        {props.teams && (
+                            props.teams.map((team, key) => (
+                                <div key={key}>
+                                    <h1 className="text-xl">{team.game}</h1>
+                                    <Link href = {'/team/' + team._id } >
+                                        <a>
+                                            <p className="text-lg mb-4"><span className="pl-2">{team.team_name}</span></p>
+                                        </a>
+                                    </Link>
+                                </div>
+                            ))
+                        )} 
                     </div>
                     <div className="flex flex-col gap-2">
                         <button className="bg-yellow-500 text-gray-700 px-4 py-2 rounded-3xl font-bold inline">Join a Team</button>
-                        <button className="bg-yellow-500 text-gray-700 px-4 py-2 rounded-3xl font-bold inline">Create Team</button>
+                        <Link href="/team/createteam">
+                            <button className="bg-yellow-500 text-gray-700 px-4 py-2 rounded-3xl font-bold inline">Create Team</button>
+                        </Link>
                     </div>
                     
                 </div>
