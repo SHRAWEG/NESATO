a369266390ae6bb7f7ca3f8dbf54da1eb70ba81e
SHRAWEG <swgmaharjan@gmail.com>
2021-08-02 15:15:08 +0545

    Search Users update

--- src/components/team/teamprofile.js
+++ src/components/team/teamprofile.js
@@ -2,13 +2,13 @@ import React from 'react';
 import SearchPlayers from './elements/searchplayers';
 
 function TeamProfile(props) {
-        
+
     return (
             <>
                 <div className ="container mx-auto flex justify-evenly whitespace-nowrap gap-x-5 mb-10">
                     <div className="bg-white  h-auto rounded-2xl whitespace-normal px-10 py-5 w-8/12 mt-32" id="midMain">
                         {props.user._id == props.team.team_cap && (
-                            <SearchPlayers user={props.user} team={props.team} users={props.users}/> 
+                            <SearchPlayers user={props.user} team={props.team} users={props.users} invitations={props.invitations} /> 
                         )}              
                     </div>   
                 </div> 
